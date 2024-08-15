b7c1281de27e5b0224f21572a90ece4c1752b48e
DrkD3vil <bijoydeyemon3@gmail.com>
2024-08-15 17:31:22 +0600

    backend complete

--- JOJOBI/app/Http/Controllers/HomeController.php
+++ JOJOBI/app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -9,6 +11,15 @@ class HomeController extends Controller
     // Index Page Admin
     public function index()
     {
-        return view('admin.index');
+        $totalUsers = User::count();
+        $totalProducts = Product::count();
+        $targetNumber = 100; // Set your target number here
+
+        // Calculate progress percentage and ensure it doesn't exceed 100%
+        $progressPercentages = [
+            'users' => min(($totalUsers / $targetNumber) * 100, 100),
+            'products' => min(($totalProducts / $targetNumber) * 100, 100),
+        ];
+        return view('admin.index', compact('totalUsers', 'totalProducts','progressPercentages'));
     }
 }
